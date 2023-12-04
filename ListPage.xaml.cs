ab98b2e3d3b25bd6505767d990327fc3cd76c6a2
AndreeaDCa <andreea.dindelegan@stud.ubbcluj.ro>
2023-12-04 23:13:11 +0200

    Actualizare

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -4,10 +4,10 @@ namespace Dindelegan_Andreea_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -21,5 +21,40 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
 
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    private async Task OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button)
+        {
+            
+            if (button.BindingContext is Product product)
+            {
+                var confirmation = await DisplayAlert("Confirmare", $"Doriti sa stergeti produsul: {product.Nume}?", "Da", "Nu");
+
+                if (confirmation)
+                {
+                    
+                    await App.Database.DeleteProductAsync(product);
+
+                    var shopList = (ShopList)BindingContext;
+                    listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+                }
+            }
+        }
+    }
 }
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dindelegan_Andreea_Lab7.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+        public object Nume { get; internal set; }
+    }
+
+}
